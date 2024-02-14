25825e099a134aefd790c94709249692706fe12b
sharedordaz <sharedordaz@live.com.mx>
2024-02-14 01:15:31 -0600

    show skeleton working

--- cli/main.py
+++ cli/main.py
@@ -1,17 +1,7 @@
 import argparse
 import subprocess
 
-def run_command(command):
-    try:
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        if result.returncode == 0:
-            print("Command output:")
-            print(result.stdout)
-        else:
-            print("Command failed with error:")
-            print(result.stderr)
-    except Exception as e:
-        print("An error occurred:", e)
+
 
 def main():
     title = "Awesome-config"
@@ -22,16 +12,36 @@ def main():
             description=description
             )
 
-    parser.add_argument ("-show", help= "Show a parameter in the config, by default displays all default options ")
-    parser.add_argument ("-set", help="Change a parameter in the config")
-    parser.add_argument("terminal", help="default terminal", nargs=1)
+
+    parser.add_argument ("-set", help="Change a parameter in the config", nargs=2 )
+    parser.add_argument ("-show", help= "Show a parameter in the config, by default displays all default options",nargs=1 )
+    parser.add_argument ("terminal", help="Default terminal", nargs="?")
+    parser.add_argument ("editor", help="Default editor", nargs='?')
+
+    parser.parse_args()
 
     args = parser.parse_args()
 
-    command = " ".join(args.command)
+    parametersList = ["terminal", "editor"]
 
-    run_command(command)
+    if args.show:
+        if args.show[0] in parametersList:
+            print(args.show[0])
+        else:
+            print(args.show[0],"is not valid. Please enter a valid parameter ")
+    elif args.set:
+        print(args.set)
+        if args.terminal:
+            print("TERMINAL: ",args.terminal)
 
+        elif args.editor:
+            print(args.editor)
+        else:
+            print("Please specify " + args.set[0] + " value")
+    else:
+        print("Please specify a valid command\nWrite \033[0;1mawesome-config -h\033[0m for help")
+
+    print ("ARGUMENTS: ", args)
 
 if __name__ == "__main__":
     main()
